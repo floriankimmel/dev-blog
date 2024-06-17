936b416eeb78d0d91f1a188f1d0f51b9b24e5722
Florian Kimmel <florianmkimmel@gmail.com>
2024-06-17 15:07:50 +0200

    Use npm to build project
    Getting rid of all yarn

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  author: "Florian Kimmel",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "Florians Dev Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: false,
   postPerPage: 3,
